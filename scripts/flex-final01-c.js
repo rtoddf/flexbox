acd88f182ccdf040fbc8be93c7bed224362ddcdc
Todd Fleeman <rtoddf@comcast.net>
2019-03-16 11:50:36 -0400

    cleanup, etc

--- scripts/flex-final01-c.js
+++ scripts/flex-final01-c.js
@@ -1,6 +1,6 @@
 var photoSource = 'photos',
-	columnLimit = parseInt($('.flex-row').attr('data-columnLimit')),
-	rowLimit = parseInt($('.flex-row').attr('data-rowLimit')),
+	columnLimit = parseInt($('.grid').attr('data-columnLimit')),
+	rowLimit = parseInt($('.grid').attr('data-rowLimit')),
 	divs
 
 var Grid = new function(){
@@ -14,24 +14,24 @@ var Grid = new function(){
 		photos.sort(function() { return 0.5 - Math.random() });
 
 		for(var i=0; i < (columnLimit*rowLimit); i++){
-			$('.flex-row').append( _.template(template_raw, { members: photos[i] }) )
+			$('.grid').append( _.template(template_raw, { members: photos[i] }) )
 		}
 
 		if(rowLimit != 1){
-			divs = $('.flex-row>.flex-item')
+			divs = $('.grid>div')
 
 			for(var i = 0; i < divs.length; i+=rowLimit) {
-				divs.slice(i, i+rowLimit).wrapAll('<div class="flex-item" data-layout="column"></div>');
+				divs.slice(i, i+rowLimit).wrapAll('<div data-layout="column"></div>');
 			}
 		}
 	}
 }
 
-var template_raw = '<div class="flex-item" data-image-layout="square"> \
+var template_raw = '<div data-image-layout="square"> \
 	<div class="tile"> \
-		<div class="image-holder"> \
+		<figure> \
 			<img src="../../images/<%= members.image %>" alt="<%= members.name %>"" name="<%= members.name %>""> \
-		</div> \
+		</figure> \
 		<div class="content-holder"> \
 			<h4><%= members.name %></h4> \
 		</div> \
